04634b03473bd1e3267b653857804fb2167bdc2f
amineBouzouita <76960437+amineBouzouita@users.noreply.github.com>
2023-08-31 12:47:50 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello  ASfvBOCHne</h1>
+</body>
+</html>
+<?php
+$servername = "localhost";
+$username = "myuser";
+$password = "mypassword";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
